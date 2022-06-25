28550338cb4ada83398512254814375669a6cede
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-06-25 20:42:57 +0200

    home

--- src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/controllers/SucursalController.java
+++ src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/controllers/SucursalController.java
@@ -1,5 +1,8 @@
 package cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t01.n01.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class SucursalController {
 
 }
