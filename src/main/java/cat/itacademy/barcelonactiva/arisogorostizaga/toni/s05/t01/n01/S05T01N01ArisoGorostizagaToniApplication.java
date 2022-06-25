fcb6ef48287fb36997c115864f632d96330bb819
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-06-25 20:04:45 +0200

    objectius

---
+++ src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/S05T01N01ArisoGorostizagaToniApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t01.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T01N01ArisoGorostizagaToniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T01N01ArisoGorostizagaToniApplication.class, args);
+	}
+
+}
