d604a2b2983b22dd0de7fce64390075a9e634445
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-06-26 21:30:31 +0200

    unimplemented methods service

--- src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/model/services/SucursalServiceImpl.java
+++ src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/model/services/SucursalServiceImpl.java
@@ -1,5 +1,43 @@
 package cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t01.n01.model.services;
 
-public class SucursalServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t01.n01.model.domain.Sucursal;
+import cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t01.n01.model.dto.SucursalDTO;
+
+@Service
+public class SucursalServiceImpl implements SucursalService {
+
+	@Override
+	public SucursalDTO convertEntityToDTO(Sucursal sucursal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SucursalDTO> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SucursalDTO getOne(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(SucursalDTO sucursal) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
