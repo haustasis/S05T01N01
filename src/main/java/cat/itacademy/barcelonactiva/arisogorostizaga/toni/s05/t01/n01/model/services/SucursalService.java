74a2abb1eda69a301859d36fece37e2f76064a0a
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-06-26 21:27:46 +0200

    Interface Sucursal Service

--- src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/model/services/SucursalService.java
+++ src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/model/services/SucursalService.java
@@ -1,7 +1,17 @@
 package cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t01.n01.model.services;
 
+import java.util.List;
+
+import cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t01.n01.model.domain.Sucursal;
+import cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t01.n01.model.dto.SucursalDTO;
+
 public interface SucursalService {
 	
-	public List<Sucursal> getAll();
+	public SucursalDTO convertEntityToDTO(Sucursal sucursal);
+	public List<SucursalDTO> getAll();
+	public SucursalDTO getOne(Integer id);
+	public void delete(Integer id);
+	public void add(SucursalDTO sucursal);
+	
 
 }
