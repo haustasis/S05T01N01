8dc3dc604ddf0fd56a1ed5b958e722e047d3bdd2
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-06-26 21:21:43 +0200

    sucursalDTO

--- src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/model/dto/SucursalDTO.java
+++ src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/model/dto/SucursalDTO.java
@@ -1,5 +1,90 @@
 package cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t01.n01.model.dto;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SucursalDTO {
+
+	private Integer pk_SucursalID;
+	private String nomSucursal;
+	private String paisSucursal;
+	private String tipusSucursal;
+	
+	private List<String> paisos = Arrays.asList("Alemania", "Belgica", "Croacia", "Dinamarca", "España", "Francia", "Irlanda", "Letonia", "Luxemburgo", "Paises Bajos", "Suecia", "Bulgaria", "Eslovaquia", "Estonia", "Grecia", "Malta", "Polonia", "Republica Checa", "Austria", "Chipre", "Eslovenia", "Finlandia", "Hungria", "Italia", "Lituania", "Portugal", "Rumania");
+
+
+	public SucursalDTO() {
+		
+	}
+
+
+	public SucursalDTO(Integer pk_SucursalID, String nomSucursal, String paisSucursal, String tipusSucursal,
+			List<String> paisos) {
+		super();
+		this.pk_SucursalID = pk_SucursalID;
+		this.nomSucursal = nomSucursal;
+		this.paisSucursal = paisSucursal;
+		this.tipusSucursal = tipusSucursal;
+		this.paisos = paisos;
+	}
+
+
+	public Integer getPk_SucursalID() {
+		return pk_SucursalID;
+	}
+
+
+	public void setPk_SucursalID(Integer pk_SucursalID) {
+		this.pk_SucursalID = pk_SucursalID;
+	}
+
+
+	public String getNomSucursal() {
+		return nomSucursal;
+	}
+
+
+	public void setNomSucursal(String nomSucursal) {
+		this.nomSucursal = nomSucursal;
+	}
+
+
+	public String getPaisSucursal() {
+		return paisSucursal;
+	}
+
+
+	public void setPaisSucursal(String paisSucursal) {
+		this.paisSucursal = paisSucursal;
+	}
+
+
+	public String getTipusSucursal() {
+		return tipusSucursal;
+	}
+
+
+	public void setTipusSucursal(String tipusSucursal) {
+		this.tipusSucursal = tipusSucursal;
+	}
+
+
+	public List<String> getPaisos() {
+		return paisos;
+	}
+
+
+	public void setPaisos(List<String> paisos) {
+		this.paisos = paisos;
+	}
+
+
+	@Override
+	public String toString() {
+		return "SucursalDTO [pk_SucursalID=" + pk_SucursalID + ", nomSucursal=" + nomSucursal + ", paisSucursal="
+				+ paisSucursal + ", tipusSucursal=" + tipusSucursal + ", paisos=" + paisos + "]";
+	}
+	
+	
 
 }
--- src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/model/services/SucursalService.java
+++ src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/model/services/SucursalService.java
@@ -1,5 +1,7 @@
 package cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t01.n01.model.services;
 
 public interface SucursalService {
+	
+	public List<Sucursal> getAll();
 
 }
