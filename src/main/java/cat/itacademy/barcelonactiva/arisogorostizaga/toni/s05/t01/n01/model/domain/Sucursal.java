fcf5071e86909a5a556f693db9fcc36af0d42096
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-06-26 21:01:21 +0200

    sucursal

--- src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/model/domain/Sucursal.java
+++ src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t01/n01/model/domain/Sucursal.java
@@ -1,5 +1,67 @@
 package cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t01.n01.model.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sucursales")
 public class Sucursal {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer pk_SucursalID;
+	
+	@Column(name="nombre", nullable = false, length = 80, unique = true)
+	private String nomSucursal;
+	
+	@Column(name="pais", nullable = false, length = 40)
+	private String paisSucursal;
+	
+	public Sucursal() {
+		
+	}
+
+	public Sucursal(Integer pk_SucursalID, String nomSucursal, String paisSucursal) {
+		super();
+		this.pk_SucursalID = pk_SucursalID;
+		this.nomSucursal = nomSucursal;
+		this.paisSucursal = paisSucursal;
+	}
+
+	public Integer getPk_SucursalID() {
+		return pk_SucursalID;
+	}
+
+	public void setPk_SucursalID(Integer pk_SucursalID) {
+		this.pk_SucursalID = pk_SucursalID;
+	}
+
+	public String getNomSucursal() {
+		return nomSucursal;
+	}
+
+	public void setNomSucursal(String nomSucursal) {
+		this.nomSucursal = nomSucursal;
+	}
+
+	public String getPaisSucursal() {
+		return paisSucursal;
+	}
+
+	public void setPaisSucursal(String paisSucursal) {
+		this.paisSucursal = paisSucursal;
+	}
+
+	@Override
+	public String toString() {
+		return "Sucursal [pk_SucursalID=" + pk_SucursalID + ", nomSucursal=" + nomSucursal + ", paisSucursal="
+				+ paisSucursal + "]";
+	}
+	
+	
 
 }
